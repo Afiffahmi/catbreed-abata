249eb4331088fa0c54d9edc0ba864da6a133cd8c
Afif Fahmi <afif.rafpi@gmail.com>
2023-12-21 15:46:03 +0800

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,NgxDatatableModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
-  
 })
 export class AppModule {}
--- src/app/pages/cats/cats.module.ts
+++ src/app/pages/cats/cats.module.ts
@@ -7,17 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { CatsPageRoutingModule } from './cats-routing.module';
 
 import { CatsPage } from './cats.page';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CatsPageRoutingModule,
-    NgxDatatableModule
+    CatsPageRoutingModule
   ],
-  declarations: [CatsPage],
-
+  declarations: [CatsPage]
 })
 export class CatsPageModule {}
